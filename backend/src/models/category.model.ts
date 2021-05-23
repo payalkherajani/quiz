157a3f6a6663a2c0252e21287950d3f4e358bea1
payalkherajani <payal.kherajani19@gmail.com>
2021-05-23 13:52:11 +0530

    backend:first-model-with-typescript

---
+++ backend/src/models/category.model.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from 'mongoose';
+
+//interface representing document in MongoDB
+interface Category {
+    level: string;
+    image: string;
+}
+
+const categoryModel = new Schema<Category>(
+    {
+        level: {
+            type: String,
+            required: true
+        },
+        image: {
+            type: String,
+            required: true
+        }
+
+    },
+
+    {
+        timestamps: true
+    }
+);
+
+const Categories = mongoose.model<Category>('Categories', categoryModel);
+
+export default Categories;
