8d3571fb1412558175c339f72eb6bbe3c6676267
payalkherajani <payal.kherajani19@gmail.com>
2021-06-06 12:09:01 +0530

    added-react-bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       Hello Typescript
+      <Button variant="primary">
+        flat button
+     </Button>
     </div>
   );
 }
