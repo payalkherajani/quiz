da82647abd552f761a1016b99c9e8982c60d2a3a
payalkherajani <payal.kherajani19@gmail.com>
2021-06-06 10:58:38 +0530

    add-quiz-with-its-category

--- backend/src/controllers/quiz.controller.ts
+++ backend/src/controllers/quiz.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { asyncHandler } from "../middlewares";
-import { Quiz } from "../models";
+import { Quiz, Categories } from "../models";
 import { Puzzel } from "../models/quiz.model";
 
 //@desc   Add a New Quiz
@@ -14,8 +14,12 @@ const addNewQuiz = asyncHandler(async (req: Request, res: Response) => {
         questions,
         category
     });
-    const newlyCreatedQuiz = await addQuiz.save();  //_id 
-    // put category_model call and push quiz id
+    const newlyCreatedQuiz = await addQuiz.save();
+    const categoryModel = await Categories.findOne({ _id: category });
+    categoryModel?.quizzes.push(newlyCreatedQuiz._id);
+    await categoryModel?.save();
+    res.status(200).json({ success: true, data: newlyCreatedQuiz });
+
 });
 
 export { addNewQuiz };
