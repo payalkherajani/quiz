f139eb77481677060c530d4ae18cfe3e9d47d2f2
payalkherajani <payal.kherajani19@gmail.com>
2021-06-25 15:33:24 +0530

    frontend:improved-category-card-design

--- frontend/src/components/Categories.tsx
+++ frontend/src/components/Categories.tsx
@@ -14,13 +14,12 @@ const Categories = (): JSX.Element => {
                 {
                     categories && categories.map((c: Category) => {
 
-                        return <Col key={c._id}>
-                            <Card className="bg-dark text-center">
+                        return <Col key={c._id} className="m-2">
+                            <Card className="bg-dark text-center pb-2">
                                 <Card.Img src={c.image} alt="Card image" />
                                 <Card.ImgOverlay>
-                                    <Card.Title>{c.level}</Card.Title>
-                                    <Button>PLAY</Button>
                                 </Card.ImgOverlay>
+                                <Button variant="secondary">{c.level}</Button>
                             </Card>
                         </Col>;
                     })
