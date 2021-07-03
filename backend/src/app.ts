6ad6fb2bbc56de2325e25043161af3110bb99a39
payalkherajani <payal.kherajani19@gmail.com>
2021-07-03 20:35:23 +0530

    backend:clubbed-routes-index.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,8 @@
 import express, { Express } from 'express';
 import cors from 'cors';
 import connectDB from './config/db';
-import categoryRoutes from './routes/category.route';
-import quizRoutes from './routes/quiz.route';
 import { errorHandler, NoSuchRouteExists } from './middlewares';
+import { categoryRoutes, quizRoutes, userRoutes } from './routes';
 
 const app: Express = express();
 connectDB();
@@ -17,6 +16,7 @@ app.use(express.json());  //to accept req.body
 //routes
 app.use('/api/category', categoryRoutes);
 app.use('/api/quiz', quizRoutes);
+app.use('/api/user', userRoutes);
 
 //keep this at last
 app.use(errorHandler);
