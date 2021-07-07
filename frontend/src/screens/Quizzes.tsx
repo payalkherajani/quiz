5f8f7d050699278c7b793cac7589bcf2d750fa42
payalkherajani <payal.kherajani19@gmail.com>
2021-07-07 12:34:31 +0530

    frontend:quizzes-page-designed

--- frontend/src/screens/Quizzes.tsx
+++ frontend/src/screens/Quizzes.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Jumbotron, Card, Row, Col, Container, ListGroup, Button } from 'react-bootstrap';
+import { Jumbotron, Card, Row, Col, Container, ListGroup, Button, Image } from 'react-bootstrap';
 import { useAppContext } from '../context/Context';
 import { useParams } from 'react-router';
 
@@ -10,6 +10,7 @@ import {
 import { ADD_SELECTED_QUIZ_QUESTIONS } from '../constants/constants';
 import { Question } from '../types/types';
 import { useNavigate } from 'react-router-dom';
+import game from '../assets/game.svg';
 
 
 const Quizzes = (): JSX.Element => {
@@ -32,27 +33,34 @@ const Quizzes = (): JSX.Element => {
     return (
         <>
             <Jumbotron className="bg-white">
-                <Container className="text-center">
+                <Container fluid className="text-center">
                     <h1 className="mb-4">INSTRUCTIONS</h1>
+                    <Row className="justify-content-center">
+                        <Col className="shadow mb-3 mr-1 bg-white rounded" lg="4">
+                            <Image src={game} alt="game" style={{ width: '100%' }} />
+                        </Col>
+                        <Col className="shadow mb-3 ml-1 bg-white rounded" lg="7">
+                            <ListGroup className="p-5">
+                                <ListGroup.Item variant="secondary text-left"><strong>There are total 6 questions, carrying equal weightage</strong></ListGroup.Item>
+                                <ListGroup.Item variant="secondary text-left"><strong>For every correct answer, you will be rewarded 1 point</strong></ListGroup.Item>
+                                <ListGroup.Item variant="secondary text-left"><strong>And Similary for every incorrect answer you will lose 1 point</strong></ListGroup.Item>
+                                <ListGroup.Item variant="secondary text-left"><strong>There, is no time limit</strong></ListGroup.Item>
+                            </ListGroup>
+                        </Col>
+                    </Row>
 
-                    <ListGroup className="mt-2">
-                        <ListGroup.Item variant="">There are total 6 questions, carrying equal weightage</ListGroup.Item>
-                        <ListGroup.Item variant="">For every correct answer, you will be rewarded 1 point</ListGroup.Item>
-                        <ListGroup.Item variant="">And Similary for every incorrect answer you will lose 1 point</ListGroup.Item>
-                        <ListGroup.Item variant="">There, is no time limit</ListGroup.Item>
-                    </ListGroup>
                 </Container>
             </Jumbotron>
 
-            <Row className="m-0">
+            <Row className="mb-5 justify-content-center" style={{ width: '100%' }} >
                 {
-                    quizData && quizData.map((data) => (
-                        <Col key={data._id} className="mb-3">
+                    quizData && quizData.map((data, index) => (
+                        <Col key={data._id} className="mb-3" lg="4" style={{ width: '80%' }}>
                             <Card
-                                bg='primary'
+                                bg={index % 2 === 0 ? 'danger' : 'info'}
                                 text='white'
-                                className="mb-2"
-                                style={{ width: '18rem' }}
+                                className="mb-2 ml-2 mr-2"
+                                style={{ width: '100%', alignSelf: 'center' }}
                             >
                                 <Card.Body>
                                     <Card.Title className="text-uppercase text-center">{data.quizname} </Card.Title>
