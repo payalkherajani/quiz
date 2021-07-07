5eb664d8175fc7337d710046be594aadb663e1e5
payalkherajani <payal.kherajani19@gmail.com>
2021-07-07 12:44:37 +0530

    frontend:added-svg-for-carousel

--- frontend/src/components/Carousel.tsx
+++ frontend/src/components/Carousel.tsx
@@ -1,12 +1,8 @@
 import { Carousel, Image } from 'react-bootstrap';
+import cc1 from '../assets/cc1.svg';
+import cc2 from '../assets/cc2.svg';
 
-
-const images = [
-    "https://images.pexels.com/photos/209679/pexels-photo-209679.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=500&amp;w=500",
-    "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-    "https://images.pexels.com/photos/776654/pexels-photo-776654.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260",
-    "https://images.pexels.com/photos/209728/pexels-photo-209728.jpeg?auto=compress&amp;cs=tinysrgb&amp;h=750&amp;w=1260"
-];
+const images = [cc1, cc2];
 
 const CarouselComponent = () => {
     return (
@@ -18,10 +14,11 @@ const CarouselComponent = () => {
                             className="d-block w-100 h-90"
                             src={item}
                             alt="slide"
+                            style={{ height: '500px' }}
                         />
                         <Carousel.Caption>
-                            <h1 className="text-primary">PAHELI</h1>
-                            <h5 className="text-info">#BujhoToJane</h5>
+                            <h1 className="text-primary text-right">PAHELI</h1>
+                            <h5 className="text-info text-right">#BujhoToJane</h5>
                         </Carousel.Caption>
                     </Carousel.Item>;
                 })
